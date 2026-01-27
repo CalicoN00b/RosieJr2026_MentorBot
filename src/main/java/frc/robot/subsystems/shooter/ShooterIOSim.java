c32529436768c85ee78a9a8156e881219a2d9fa1
Ian Galaviz <brianatthehouse@gmail.com>
2026-01-26 21:29:10 -0700

    no clue if this shooter sim even works or is worth it

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOSim.java
@@ -0,0 +1,41 @@
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
+public class ShooterIOSim implements ShooterIO {
+
+    private final DCMotorSim shooterMotor;
+
+    public ShooterIOSim() {
+        shooterMotor = new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(
+                DCMotor.getKrakenX60(1), 1, 1), 
+            DCMotor.getKrakenX60(1)
+        );
+    }
+
+    @Override
+    public void updateInputs(ShooterIOInputs inputs) {
+        inputs.connected = true;
+        inputs.appliedVolts = shooterMotor.getInputVoltage();
+        inputs.currentAmps = shooterMotor.getCurrentDrawAmps();
+    }
+
+    @Override
+    public void setShooterDutyCycle(double output) {
+        shooterMotor.setInputVoltage(output * 12);
+    }
+
+    @Override
+    public void setShooterVelocity(double velocityRadPerSec) {
+        shooterMotor.setAngularVelocity(velocityRadPerSec);
+    }
+
+    @Override
+    public void setShooterNeutral() {
+        shooterMotor.setInputVoltage(0);
+    }
+
+}

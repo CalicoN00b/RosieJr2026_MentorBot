80292b16679328a4799d1a3492cff191fbb7a97b
Ian Galaviz <brianatthehouse@gmail.com>
2026-01-27 21:56:36 -0700

    no clue if this shooter sim is gonna do what it's supposed to do

--- src/main/java/frc/robot/subsystems/shooter/ShooterIOSim.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOSim.java
@@ -1,39 +1,50 @@
 package frc.robot.subsystems.shooter;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 
 public class ShooterIOSim implements ShooterIO {
 
-  private final DCMotorSim shooterMotor;
+  private final DCMotor gearbox = DCMotor.getKrakenX60(1);
+  private final DCMotorSim sim;
+
+  private double currentOutput = 0.0;
+  private double appliedVolts = 0.0;
 
   public ShooterIOSim() {
-    shooterMotor =
-        new DCMotorSim(
-            LinearSystemId.createDCMotorSystem(DCMotor.getKrakenX60(1), 1, 1),
-            DCMotor.getKrakenX60(1));
+    sim = new DCMotorSim(
+      LinearSystemId.createDCMotorSystem(gearbox, 0.0141593652, 0.75), 
+      gearbox
+    );
   }
 
   @Override
   public void updateInputs(ShooterIOInputs inputs) {
+    appliedVolts = gearbox.getVoltage(currentOutput, sim.getAngularVelocityRadPerSec());
+
+    sim.setInputVoltage(MathUtil.clamp(appliedVolts, -12.0, 12.0));
+    sim.update(0.02);
+
     inputs.connected = true;
-    inputs.appliedVolts = shooterMotor.getInputVoltage();
-    inputs.currentAmps = shooterMotor.getCurrentDrawAmps();
+    inputs.appliedVolts = appliedVolts;
+    inputs.currentAmps = sim.getCurrentDrawAmps();
+    inputs.velocityRadPerSec = sim.getAngularVelocityRadPerSec();
   }
 
   @Override
   public void setShooterDutyCycle(double output) {
-    shooterMotor.setInputVoltage(output * 12);
+    sim.setInputVoltage(output * 12);
   }
 
   @Override
   public void setShooterVelocity(double velocityRadPerSec) {
-    shooterMotor.setAngularVelocity(velocityRadPerSec);
+    sim.setAngularVelocity(velocityRadPerSec);
   }
 
   @Override
   public void setShooterNeutral() {
-    shooterMotor.setInputVoltage(0);
+    sim.setInputVoltage(0);
   }
 }
